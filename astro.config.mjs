46f1eb0734f2aad679d9cc24318c4a7414b26e68
darkgalink <bryanalejandromedina@gmail.com>
2025-07-20 20:24:08 -0500

    mejora vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless'; // Importa el adaptador de Vercel serverless específicamente
+import vercel from '@astrojs/vercel/serverless'; // Importa el adaptador de Vercel
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel({
-        // Asegúra que se generen correctamente los archivos del servidor
-    includeFiles: ['./dist/server/entry.mjs']
-  }), // Usa el adaptador de Vercel
+  adapter: vercel(), // Usa el adaptador de Vercel
   integrations: [svelte(), tailwind()]
 });
